1931c9ba42bba12e3113f2b15ba7e3d939d3fb8e
estaidesoriginal <ar.benlulu@duocuc.cl>
2026-09-14 20:39:01 -0300

    pequeños fallos corregidos

--- ms-rutaexpress-audit/build.gradle.kts
+++ ms-rutaexpress-audit/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	kotlin("plugin.jpa") version "2.1.10"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("eclipse")
 }
 
 group = "com.rutaexpress"
